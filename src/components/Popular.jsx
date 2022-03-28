e13d394f7af2bdd16f7809ff81f699e76a7576a4
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-03-28 15:25:36 +1300

    Set up components, display info

---
+++ src/components/Popular.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+function Popular() {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+      const data = await api.json();
+      setPopular(data.recipes)
+      console.log(data)
+  }
+
+
+
+  return (
+    <div>
+
+      {popular.map((recipe) => {
+        return(
+          <div key={recipe.id}> 
+            <p>{recipe.title}</p>
+            {recipe.dairyFree ? <p>Dairy Free</p> : <p>Dairy</p>}
+            <p>{}</p>
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Popular
